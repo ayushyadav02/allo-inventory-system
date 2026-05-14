070678a6330c757088493f10eb6d4e63c1bd4092
Ayush Yadav <ayushrao9407@gmail.com>
2026-05-14 09:41:44 +0530

    completed

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,8 @@
 import { InventoryService } from "@/lib/services/inventory";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const data = await InventoryService.getProducts();
   return NextResponse.json(data);
